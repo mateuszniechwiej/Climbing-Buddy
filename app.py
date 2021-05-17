613e1528861ebf6a8f640534a843a228e4f49db2
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-05-17 16:40:51 +0100

    Add Procfile

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import os
 from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    """
+    Test function  
+    """
+    return "It is working"
+
+# telling app how and where to run
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)
