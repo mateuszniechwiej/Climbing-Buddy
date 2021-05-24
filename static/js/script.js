6a080bc8f46cda4338ee68041246f52c6e242e6a
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-05-24 15:35:37 +0100

    Create add_event template

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,10 @@
-// "flapickr" date and time picker 
-flatpickr('.flatpickr', {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i"
-});
+// TODO: // "flapickr" date and time picker 
+// flatpickr('.flatpickr', {
+//     enableTime: true,
+//     time_24hr: true,
+//     altInput: true,
+//     altFormat: 'd M Y H:i',
+//     dateFormat: 'Y-m-d H:i',
+
+// });
+
